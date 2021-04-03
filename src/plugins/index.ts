228d6234f0712a0ef3f001420cd26f28ab6e5904
Jaime Hernández Cruz <jhdezcruz@gmail.com>
2021-04-03 13:43:52 -0600

    UPDGRADE -  updating to 0.18.5 removing use-sku

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,7 +1,5 @@
 import { CancelOrderCustomerPlugin } from './customer-order/cancel-order/cancel-order.plugin';
-import { PublicStockPlugin } from './use-sku/use-sku.plugin';
 
 export const DJ_PLUGINS = [
     CancelOrderCustomerPlugin,
-    PublicStockPlugin
 ]
