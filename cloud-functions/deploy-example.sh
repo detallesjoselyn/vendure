7482f2f431bb2ea0a060237f240fbe439b668828
Jaime Hernández Cruz <jhdezcruz@gmail.com>
2021-04-04 12:44:20 -0500

    [FEATURE][CLOUD FUNCTINS] Adding documentation and deploy scripts

--- cloud-functions/deploy-example.sh
+++ cloud-functions/deploy-example.sh
@@ -1,4 +1,4 @@
-gcloud functions deploy dialogflowFirebaseFulfillmentProd --entry-point dialogflowFirebaseFulfillment --runtime nodejs12 --trigger-http --allow-unauthenticated \
+gcloud functions deploy dialogflowFirebaseFulfillment --entry-point dialogflowFirebaseFulfillment --runtime nodejs12 --trigger-http --allow-unauthenticated \
 --update-env-vars PAGE_ACCESS_TOKEN="*****" \ # Facebook page access token
 --update-env-vars ENVIRONMENT="***" \ # PROD or DEV
 --update-env-vars SECRET="****" # to encript
