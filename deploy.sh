b05d05c1ca36b4e65f90e9e3e7dd14d8150cb4cc
Jaime Hernández Cruz <jhdezcruz@gmail.com>
2021-04-03 22:18:37 -0600

    FEATURE - removing deploy scropt prod and fixing deploy dev

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,9 @@ gcloud run deploy ecommerce-detalles \
      --region "us-central1" \
      --allow-unauthenticated \
      --add-cloudsql-instances detallesjoselyn:us-central1:detallesjocelynstorage \
-  --update-env-vars INSTANCE_CONNECTION_NAME="detallesjoselyn:us-central1:detallesjocelynstorage" --platform managed
+     --set-env-vars DATABASE_NAME="" \
+     --set-env-vars DATABASE_USER="" \
+     --set-env-vars INSTANCE_CONNECTION_NAME="detallesjoselyn:us-central1:detallesjocelynstorage"\
+     --set-env-vars DATABASE_HOST="/cloudsql/detallesjoselyn:us-central1:detallesjocelynstorage" \
+     --set-env-vars DATABASE_PASSWORD="" \
+     --platform managed
