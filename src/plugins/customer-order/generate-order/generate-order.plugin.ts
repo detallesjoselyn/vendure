1e2fe885324177ef4cf8c3d1ca573b18356c5107
Samuel Abad <samuel.abad@kairosds.com>
2021-04-11 13:51:25 -0500

    feat: resolver and servite to handle customer order and add items

--- src/plugins/customer-order/generate-order/generate-order.plugin.ts
+++ src/plugins/customer-order/generate-order/generate-order.plugin.ts
@@ -1,11 +1,11 @@
-import { AdminUiPlugin } from '@vendure/admin-ui-plugin';
 import  gql  from 'graphql-tag';
 import { VendurePlugin, PluginCommonModule, OrderService, ShippingMethodService, SearchResolver } from "@vendure/core";
 import { GenerateOrderResolver } from './generate-order.resolver';
+import { GenerateCustomerOrderService } from './generate-customer-order.service';
 
 @VendurePlugin({
     imports: [PluginCommonModule],
-    providers: [OrderService,ShippingMethodService],
+    providers: [OrderService,ShippingMethodService,GenerateCustomerOrderService],
     shopApiExtensions: {
         schema: gql`
             extend type Mutation {
