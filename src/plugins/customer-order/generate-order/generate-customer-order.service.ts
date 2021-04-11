ed0152bad0d7deec3d04a87b522d9c83bbc90029
Samuel Abad <samuel.abad@kairosds.com>
2021-04-11 13:51:25 -0500

    feat: resolver and servite to handle customer order and add items

---
+++ src/plugins/customer-order/generate-order/generate-customer-order.service.ts
@@ -0,0 +1,51 @@
+import {
+    Injectable,
+} from '@nestjs/common';
+import { 
+    TransactionalConnection,
+    RequestContext, 
+    ProductVariant 
+} from '@vendure/core';
+
+
+
+@Injectable()
+export class GenerateCustomerOrderService {
+    private relations = ['featuredAsset', 'taxCategory', 'channels'];
+    constructor(
+        private connection: TransactionalConnection
+    ) {}
+
+    /**
+     * Search for products from an array of codes
+     * @param ctx Session context
+     * @param inCodes  Array<String> codes to find in DataBase
+     */
+    async findProductVariantsBySKU(ctx: RequestContext, inCodes: Array<String>): Promise<Array<ProductVariant>> {
+        const qb = this.connection.getRepository<ProductVariant>(ctx,ProductVariant)
+        .createQueryBuilder('ProductVariant');
+        if (inCodes.length > 0) {
+            qb.andWhere(`ProductVariant.sku IN ( :...skus )`,{skus: inCodes});
+        } else {
+            qb.andWhere("ProductVariant.sku IN ('-1')");
+        }
+        qb.andWhere('ProductVariant.deletedAt IS NULL');
+        
+        return qb.getMany();
+    }
+
+
+    /**
+     * Validate that at least one product has stock to assign
+     * @param productVariants Array<ProductVariant>
+     */
+    isStockAvailable(productVariants: Array<ProductVariant>): Boolean {
+        let isAvailable = false;
+        productVariants.map((productVariant) => {
+            if ((productVariant.stockOnHand - productVariant.stockAllocated) > 0) {
+                isAvailable = true
+            }
+        });
+        return isAvailable;
+    }
+}
